6e258d4a0ea2e98fbc46b7663debdba4b5ae3283
Tarrasques <1417223554@qq.com>
2022-07-29 00:31:10 +0800

    更新Eureka Service

--- tlog/src/main/java/com/tblog/tlog/TlogApplication.java
+++ tlog/src/main/java/com/tblog/tlog/TlogApplication.java
@@ -3,9 +3,13 @@ package com.tblog.tlog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @MapperScan("com.tblog.tlog.mapper")
+@EnableWebSecurity
+@EnableEurekaClient
 public class TlogApplication {
 
     public static void main(String[] args) {
--- tlog/src/main/java/com/tblog/tlog/controller/TestController.java
+++ tlog/src/main/java/com/tblog/tlog/controller/TestController.java
@@ -1,14 +1,12 @@
 package com.tblog.tlog.controller;
 
 import com.tblog.tlog.utils.TLog;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("test")
-@Log4j2
 public class TestController {
 
     @GetMapping("/s")
