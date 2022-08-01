748c6b2d46c04ce7a84b3cff04d205e044a0c1a3
tiamou <1417223554@qq.com>
2022-08-01 17:43:20 +0800

    AOP切面日志代码

--- tlog/src/main/java/com/tblog/tlog/comfig/SecurityConfig.java
+++ tlog/src/main/java/com/tblog/tlog/comfig/SecurityConfig.java
@@ -13,11 +13,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //       auth.userDetailsService(service);
 //    }
-//
+
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
-//        http.cors().disable();
-//        http.csrf().and().authorizeRequests().anyRequest().permitAll();
+//        String loginPageUrl = "/login";
+//        String loginUrl = "/login";
+//        String[] url = {
+//                "/",
+//                "/favicon.ico",
+//                "/browser_components/**",
+//                "/css/**",
+//                "/js/**",
+//                "/img/**",
+//                "/index.html",
+//                "/register.html",
+//                "/api-user/v1/user/reg",
+//                "/api-question/v1/tags",
+//                loginPageUrl,
+//                loginUrl
+//        };
+        //所有请求都需要验证
+//        http.authorizeRequests().antMatchers(url).permitAll().anyRequest().authenticated();
+        //使用表单验证
+//        http.formLogin().loginPage(loginPageUrl).loginProcessingUrl(loginUrl);
+        //关闭跨域
+//        http.csrf().disable();
 //    }
 
 }
